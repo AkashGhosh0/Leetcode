0eb9a71c57ebdd8b2e25ea9047820d8d77769302
Akash Ghosh <128056453+AkashGhosh0@users.noreply.github.com>
2026-06-30 14:08:19 +0530

    Add files via upload

---
+++ Leet33.java
@@ -0,0 +1,28 @@
+class leet33 {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int x = high - target;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] > target){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && nums[high] >= target){
+                    low = mid + 1;
+                }else{
+                    high = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
